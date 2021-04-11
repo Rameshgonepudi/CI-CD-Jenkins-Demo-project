f34c0474a15ea55b2e2a238fdb1e6ed0ad25a986
Ramesh <Ramesh@192.168.0.8>
2021-04-11 12:55:01 +0530

    #Commit For Jenkins Demo

---
+++ Jenkins_Project/src/main/Linkedin_Automation.java
@@ -0,0 +1,66 @@
+package main.java.testing;
+
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class Linkedin_Automation {
+	static WebDriver driver;
+	public static void main(String[] args) throws InterruptedException {
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ramesh\\Downloads\\chromedriver.exe");  
+		
+		
+        
+        // Instantiate a ChromeDriver class.     
+   driver=new ChromeDriver();  
+   driver.manage().deleteAllCookies();
+      // Launch Website  
+   driver.get("https://www.linkedin.com/uas/login?session_redirect=https%3A%2F%2Fwww%2Elinkedin%2Ecom%2Fsearch%2Fresults%2Fpeople%2F%3FgeoUrn%3D%255B%2522103644278%2522%255D%26keywords%3DA%26origin%3DFACETED_SEARCH&fromSignIn=true&trk=cold_join_sign_in"); 
+
+   driver.manage().window().maximize();
+   
+   driver.findElement(By.xpath(".//*[@id='username']")).sendKeys("-");
+   
+   driver.findElement(By.xpath(".//*[@id='password']")).sendKeys("-");
+   
+   driver.findElement(By.xpath("(.//*[text()='Sign in'])[2]")).click();
+   
+   for (int l=0; l<100;l++){
+	   
+   
+   
+   Thread.sleep(6000);
+	 
+   List<WebElement> elements = driver.findElements(By.xpath(".//*[@class='pv2 artdeco-card ph0 mb2']"));
+   for (int i=0; i<elements.size();i++){
+	      System.out.println("Radio button text:" + elements.get(i).getText());
+	    }
+   scroll();
+  
+	  }
+   
+   
+	}
+	public static void scroll() throws InterruptedException
+	{
+		
+		 JavascriptExecutor js = (JavascriptExecutor) driver;
+
+	        // Launch the application		
+
+	        //This will scroll the web page till end.		
+	        js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+	        Thread.sleep(3000);
+	        driver.findElement(By.xpath(".//*[text()='Next']")).click();
+	        Thread.sleep(3000);
+	}
+	
+	
+}
---
+++ Jenkins_Project/src/main/java/main/java/testing/OptionChainDeleteCookies.java
@@ -0,0 +1,115 @@
+package main.java.testing;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OptionChainDeleteCookies {
+	static WebDriver driver;
+	
+	@Test
+	public void OptionChainData() throws InterruptedException, FileNotFoundException {
+		
+		
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\Ramesh\\Downloads\\chromedriver.exe");  
+		
+		
+        
+        // Instantiate a ChromeDriver class.     
+   driver=new ChromeDriver();  
+     
+      // Launch Website  
+   driver.navigate().to("https://www.nseindia.com/option-chain"); 
+   
+   driver.manage().window().maximize();
+   
+   driver.manage().deleteAllCookies();
+   
+   driver.findElement(By.id("equity_optionchain_select")).click();
+    
+    
+    
+    driver.manage().deleteAllCookies();
+    String BANKNIFTY;
+    
+    driver.findElement(By.xpath("//*[contains(text(),'BANKNIFTY')]")).click();
+    driver.manage().deleteAllCookies();
+    
+    Thread.sleep(3000);
+    
+  //  driver.findElement(By.id("downloadOCTable")).click();
+    driver.findElement(By.id("downloadOCTable")).click();
+    Thread.sleep(3000);
+   // rename();
+
+	}
+	/*
+public static void rename() throws FileNotFoundException
+{
+	
+
+	File sdcard = new File("C:\\Users\\Ramesh\\Downloads\\");
+	
+	File folder = new File("C:\\Users\\Ramesh\\Downloads\\");
+	
+	
+	
+	File from = new File(sdcard,"option-chain-ED-BANKNIFTY-01-Apr-2021.csv");
+	File to = new File(folder,"to.csv");
+	from.renameTo(to);
+	write();
+	}
+public static void write() throws FileNotFoundException 
+{ FileInputStream fil;
+Workbook wb = null; 
+	try
+  	{
+  	fil=new FileInputStream("C:\\Users\\Ramesh\\Downloads\\to.csv");
+     wb=WorkbookFactory.create(fil);
+    }catch(Exception ex)
+  	{
+  		ex.printStackTrace();
+  
+  	}
+	
+	
+	
+	
+	
+	
+	
+String filepath= ("C:\\Users\\Ramesh\\Downloads\\to.csv");
+File file= new File(filepath);
+FileOutputStream fos=new FileOutputStream(file);
+   
+Workbook wbq= new XSSFWorkbook();
+   
+
+int u=0;
+int row= wb.getSheetAt(0).getLastRowNum();
+
+
+// Setting cell formula and cell type
+Cell cell = null;
+cell.setCellFormula("SUM(B2:B95)");
+//setCellFormula(SUM(B2:B95)), 
+//cell.setCellType(Cell.CELL_TYPE_FORMULA);
+
+
+
+}
+*/
+}
